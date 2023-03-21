f019008bc260e973e2935bc81a975fac1a9c0995
Lucas Wilker <lwilker24@gmail.com>
2023-03-21 15:14:31 -0300

    Renomeando variável Flask e arquivo

--- application.py
+++ application.py
@@ -2,23 +2,23 @@ from flask import Flask, request
 
 import cpf_service
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/validator/<string:cpf>', methods=['GET'])
+@application.route('/cpf/validator/<string:cpf>', methods=['GET'])
 def validate_cpf(cpf):
     return cpf_service.validate(cpf)
 
 
-@app.route('/validator/generate', methods=['POST'])
+@application.route('/cpf/generate', methods=['POST'])
 def generate_cpf():
     return cpf_service.generate_cpf()
 
 
-@app.route('/validator/generatelist', methods=['POST'])
+@application.route('/cpf/generatelist', methods=['POST'])
 def generate_list_cpf():
     return cpf_service.generate_list_cpfs(request.args.get('qtd', type=int))
 
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
